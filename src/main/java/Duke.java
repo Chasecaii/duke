ad9a7a6dd8bd86b4f3865d01ec6858f19a583543
Chasecaii <114788737+Chasecaii@users.noreply.github.com>
2025-09-14 17:07:39 +0800

    Level-0: greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String bot_name = "Chase Bot";
+        System.out.println("Hello! I'm " + bot_name);
+        System.out.println("What can I do for you?");
+
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            String line = sc.nextLine();
+            if (line.equals("bye")){
+                System.out.println("Bye. Hope to See you again soon!");
+                break;
+            }
+        }
+        sc.close();
     }
 }
